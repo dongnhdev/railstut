5a5239575a19e20b7ed5462f75fda6e26ee959d5
Hung Dong Ngo <dongnh@zigexn.vn>
2017-03-10 15:23:24 +0700

    Finish signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def new
+  	#debugger
+  	@user = User.new
   end
+  def show
+  	@user = User.find(params[:id])
+  	#debugger
+  end
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome to the sample app"
+  		redirect_to user_url(@user)
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'users/new'
 
   get '/home', to: 'static_pages#home'
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
